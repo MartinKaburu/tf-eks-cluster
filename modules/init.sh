14969bf93cdd6dec6814fe70ac5dcd9143f9b928
martinkaburu <mkaburu@worldremit.com>
2025-02-19 23:01:45 +0300

    FT: update init script

---
+++ modules/init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+main() {
+    # eks module
+    cd ./modules/eks
+
+    terraform init
+
+    terraform plan
+    terraform apply -auto-approve
+    
+    if [ $? -ne 0 ]; then # Probably failed because OIDC did not exist
+        terraform apply -auto-approve
+    fi
+    
+    # k8s module
+    read -p "Update k8s variables if necessary, then press Enter to continue..."
+    
+    cd ../../k8s
+    terraform init
+
+    terraform plan -target=helm_release.karpenter_crd
+    terraform apply -target=helm_release.karpenter_crd -auto-approve
+
+    terraform plan
+    terraform apply -auto-approve
+
+    echo "Completed successfully!"
+}
+
+main
