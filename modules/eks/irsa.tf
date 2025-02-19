898989ddf0fa7824eb1399c6a942fa97e90dc10b
martinkaburu <mkaburu@worldremit.com>
2025-02-19 22:39:22 +0300

    FT: add IAM permissions for iam:PassRole

--- modules/eks/irsa.tf
+++ modules/eks/irsa.tf
@@ -16,10 +16,11 @@ module "eks_karpenter_namespaces_sa_role" {
   }
 
   role_policy_arns = {
-    AmazonEC2FullAccess     = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-    AmazonSSMReadOnlyAccess = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+    AmazonEC2FullAccess       = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    AmazonSSMReadOnlyAccess   = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
     AmazonEKSWorkerNodePolicy = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-    AmazonEKSClusterPolicy  = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    AmazonEKSClusterPolicy    = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    AmazonIAMFullAccess       = "arn:aws:iam::aws:policy/AmazonIAMFullAccess"
   }
 
   depends_on = [module.eks]
